650a1dcc827475b163280960a2b680dc3586b338
Haytham Salama <haythamasalama@gmail.com>
2023-05-24 21:09:14 +0300

    feat(screens): chnage colors style and padding

--- lib/screens/create_question_screen.dart
+++ lib/screens/create_question_screen.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/components/answer_input.dart';
 import 'package:quiz_app/components/correct_answer_drop_down.dart';
 import 'package:quiz_app/components/question_input.dart';
+import 'package:quiz_app/constants.dart';
 import 'package:quiz_app/models/question.dart';
-import 'package:sqflite/sqflite.dart';
-
 import '../utilities/sqflite.dart';
 
 class CreateQuestionScreen extends StatefulWidget {
@@ -46,70 +45,74 @@ class _CreateQuestionScreenState extends State<CreateQuestionScreen> {
         ),
         centerTitle: true,
       ),
-      body: ListView(
-        children: [
-          QuestionInput(controller: questionController),
-          AnswerInput(
-            labelText: 'First Answer',
-            circleLetter: 'A',
-            circleColor: Colors.yellow,
-            controller: firstController,
-          ),
-          AnswerInput(
-            labelText: 'Second Answer',
-            circleLetter: 'B',
-            circleColor: Colors.green,
-            controller: secondAnswerController,
-          ),
-          AnswerInput(
-            labelText: 'Third Answer',
-            circleLetter: 'C',
-            circleColor: Colors.grey,
-            controller: thirdAnswerController,
-          ),
-          AnswerInput(
-            labelText: 'Fourth Answer',
-            circleLetter: 'D',
-            circleColor: Colors.red,
-            controller: forthAnswerController,
-          ),
-          CorrectAnswerDropDown(
-            dropDownValue: dropDownValue,
-            onChanged: (val) {
-              setState(() {
-                dropDownValue = val ?? 0;
-              });
-            },
-          ),
-          ElevatedButton(
-            onPressed: () {
-              final question = Question(
-                title: questionController.text,
-                options: [
-                  firstController.text,
-                  secondAnswerController.text,
-                  thirdAnswerController.text,
-                  forthAnswerController.text,
-                ],
-                correctAnswer: dropDownValue - 1,
-              );
-
-              addQuestion(question);
-
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(const SnackBar(content: Text("Question Added ✨")));
-
-              Navigator.of(context).pushNamed("/create-quiz");
-            },
-            child: const Text(
-              "Add Question",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 25,
-              ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: ListView(
+          children: [
+            QuestionInput(controller: questionController),
+            AnswerInput(
+              labelText: 'First Answer',
+              circleLetter: 'A',
+              circleColor: warningColor,
+              controller: firstController,
             ),
-          )
-        ],
+            AnswerInput(
+              labelText: 'Second Answer',
+              circleLetter: 'B',
+              circleColor: successColor,
+              controller: secondAnswerController,
+            ),
+            AnswerInput(
+              labelText: 'Third Answer',
+              circleLetter: 'C',
+              circleColor: secondaryColor,
+              controller: thirdAnswerController,
+            ),
+            AnswerInput(
+              labelText: 'Fourth Answer',
+              circleLetter: 'D',
+              circleColor: dangerColor,
+              controller: forthAnswerController,
+            ),
+            CorrectAnswerDropDown(
+              dropDownValue: dropDownValue,
+              onChanged: (val) {
+                setState(() {
+                  dropDownValue = val ?? 0;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                final question = Question(
+                  title: questionController.text,
+                  options: [
+                    firstController.text,
+                    secondAnswerController.text,
+                    thirdAnswerController.text,
+                    forthAnswerController.text,
+                  ],
+                  correctAnswer: dropDownValue - 1,
+                );
+
+                addQuestion(question);
+
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(const SnackBar(content: Text("Question Added ✨")));
+
+                Navigator.of(context).pushNamed("/create-quiz");
+              },
+              child: const Text(
+                "Add Question",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
